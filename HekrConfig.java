67b2a44bcd88d051e57508e04eb3ff4d7f9c8fbf
xiaoxiaomao <zhaoxing.mao@hekr.me>
2015-11-06 13:48:03 +0800

    smartConfig_update
    smartConfig_update

--- HekrConfig.java
+++ HekrConfig.java
@@ -1,208 +1,215 @@
+/**
+ *
+ * Copyright (c) Hekr.me Co.ltd Hangzhou, 2013-2015
+ * 
+ * 杭州第九区科技（氦氪科技）有限公司拥有该文件的使用、复制、修改和分发的许可权
+ * 如果你想得到更多信息，请访问 <http://www.hekr.me>
+ *
+ * Hekr Co.ltd Hangzhou owns permission to use, copy, modify and
+ * distribute this documentation.
+ * For more information, please see <http://www.hekr.me>
+ * 
+ */
+
 package com.hekr.android.app.util;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
 import com.alibaba.fastjson.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.List;
-
 /**
- * Created by kj on 15/6/18.
+ * 
+ * HekrConfig.java created by durianskh at Oct 30, 2015 6:49:19 PM 这里对类或者接口作简要描述
+ *
+ * @author durianskh
+ * @version 1.0
+ * 
  */
 
 public class HekrConfig {
 
-    public static int unsignedByteToInt(byte b) {
-        return (int) b & 0xFF;
+	/**
+	 * 原子变量，决定是否需要继续发送ssid和password
+	 */
+	private AtomicBoolean isSsidAndPassOK = new AtomicBoolean(false);
+
+	/**
+	 * 原子变量，决定是否需要继续发送ak
+	 */
+	private AtomicBoolean isAccessKeyOK = new AtomicBoolean(false);
+
+	/**
+	 * 原子变量，判断线程是否全部停止
+	 */
+	private AtomicBoolean done = new AtomicBoolean(false);
+
+	/**
+	 * 原子变量，返回的最终结果
+	 */
+	private AtomicReference<DatagramPacket> result = null;
+
+	/**
+	 * dak
+	 */
+	private final String ak;
+
+	public HekrConfig(String accesskey) {
+		this.ak = accesskey;
+	}
+
+	/**
+	 * 获取发送经历的时间
+	 * 
+	 * @param beginTime
+	 * @return
+	 */
+	private long getPassTime(long beginTime) {
+		return System.currentTimeMillis() - beginTime;
+	}
+
+	/**
+	 * 获取需要等待的时间，该变量变化不大，有时间可以多测试，写一个更好的公式
+	 * 
+	 * @param passTime
+	 * @param length
+	 * @return
+	 */
+	private long getSleepTime(long passTime, int length) {
+		long param = passTime / 1000 - 3 > 0 ? passTime / 1000 - 3 : 0;
+		long time = 100 / length * (1 + param / 6);
+		return time;
+	}
+
+	/**
+	 * 具体执行配置的过程，启动两个线程，一个线程用于发送ssid和pass，一个线程用于发送ak
+	 * 
+	 * @param ssid
+	 * @param password
+	 * @return
+	 */
+	public Object config(final String ssid, final String password) {
+
+		// 发送ssid和pass的线程
+		new Thread() {
+			public void run() {
+				byte[] data = (ssid + '\0' + password + '\0').getBytes();
+				long beginTime = System.currentTimeMillis();
+				long passTime = getPassTime(beginTime);
+				while (!isSsidAndPassOK.get()) {
+					long sleepTime;
+					if (passTime > 1000) {
+						sleepTime = getSleepTime(passTime, data.length);
+					} else {
+						sleepTime = getSleepTime(passTime, data.length + 1);
+					}
+					try {
+						UDPConfig.hekrconfig(ssid + "", password + "", (int) sleepTime);
+						Thread.sleep(sleepTime);
+					} catch (Exception e) {
+					}
+                    passTime = getPassTime(beginTime);
+				}
+			}
+		}.start();
+
+		// 发送ak的线程
+		new Thread() {
+			public void run() {
+				DatagramPacket dp;
+				while (!isAccessKeyOK.get()) {
+					try {
+						// 这里使用的是固定的时间
+						dp = UDPConfig.waitDevice(200);
+						String str = new String(dp.getData());
+						if (str.startsWith("(deviceACK ")) {
+							result = new AtomicReference<DatagramPacket>(dp);
+							isSsidAndPassOK.set(true);
+							isAccessKeyOK.set(true);
+							done.set(true);
+						}
+					} catch (IOException e) {
+						try {
+							// 这个80毫秒没有卵用
+							dp = UDPConfig.setAccessKey("255.255.255.255", ak, null, 80);
+							String str = new String(dp.getData());
+							if (str.startsWith("(deviceACK ")) {
+								result = new AtomicReference<DatagramPacket>(dp);
+								isSsidAndPassOK.set(true);
+								isAccessKeyOK.set(true);
+								done.set(true);
+							}
+						} catch (IOException e1) {
+						}
+					}
+				}
+				if (!done.get()) {
+					result = null;
+					done.set(true);
+				}
+			}
+		}.start();
+
+		int count = 0;
+		try {
+			// 120*500毫秒是1分钟，这是配置的总体超时时间
+			// 在1分钟内，不断去判断配置是否成功
+			while ((!isSsidAndPassOK.get() || !isAccessKeyOK.get()) && count < 120) {
+				// 每次判断之后，主线程休眠500毫秒
+				Thread.sleep(500);
+				count++;
+			}
+		} catch (Exception e) {
+		}
+
+		// timeout, stopping the two threads
+		isSsidAndPassOK.set(true);
+		isAccessKeyOK.set(true);
+		// 等待所有的线程停止
+		while (!done.get()) {
+		}
+
+		return result;
+	}
+
+    public void stop() {
+        isSsidAndPassOK.set(true);
+        isAccessKeyOK.set(true);
     }
 
-    public static DatagramPacket[] encode(String data) throws IOException{
-        DatagramSocket ds = new DatagramSocket();// 创建用来发送数据报包的套接字
-        int len = data.getBytes("utf-8").length;
-        DatagramPacket[] dps = new DatagramPacket[ len ];
-        byte[] bs = data.getBytes();
-        DatagramPacket  dp;
-        for(int i=0 ; i<len ; i++ ){
-            dp = new DatagramPacket( bs , len ,InetAddress.getByName("224."+i+"."+ unsignedByteToInt(bs[i]) +".255" ) , 7001 );
-            dps[i] = dp;
-            ds.send(dp);
-        }
-        dp = new DatagramPacket( bs, len , InetAddress.getByName("224."+127+"."+len+".255" ) , 7001 );
-        ds.send(dp);
-        ds.close();
-        return dps;
-    }
+	/**
+	 * hekrconfig配置
+	 * 
+	 * @param ssid
+	 * @param password
+	 * @throws java.io.IOException
+	 */
+	public static void hekrconfig(String ssid, String password) throws IOException {
+		UDPConfig.hekrconfig(ssid, password, 0);
+	}
 
-    /**
-     * hekrconfig配置
-     * @param ssid
-     * @param password
-     * @throws IOException
-     */
-    public static void hekrconfig(String ssid,String password ) throws IOException{
-        hekrconfig(ssid, password, 0);
-    }
+	public static boolean isSuccess(String respstr) {
+		try {
+			if (respstr != null) {
+				JSONObject jo = (JSONObject) JSONObject.parse(respstr);
+				int code = jo.getInteger("code");
+				if (code == 0) {
+					return true;
+				}
+			}
+			return false;
+		} catch (Exception ex) {
+			return false;
+		}
+	}
 
-    public static void hekrconfig(String ssid,String password,int time) throws IOException{
-        DatagramSocket ds = new DatagramSocket();// 创建用来发送数据报包的套接字
-        byte[] ssidbs = ssid.getBytes("utf-8");
-        byte[] passbs = password.getBytes("utf-8");
-        int len = ssidbs.length+passbs.length+2;
-        byte[] d = "hekrconfig".getBytes("utf-8");
-        DatagramPacket  dp;
-        dp = new DatagramPacket( d  , d.length , InetAddress.getByName("224.127."+len+".255" ) , 7001 );
-        ds.send(dp);
-
-        byte[] data = (ssid+'\0'+password+'\0').getBytes() ;
-
-        for(int i=0 ; i<data.length ; i++ ){
-            dp = new DatagramPacket( d  , d.length,InetAddress.getByName("224."+i+"."+ unsignedByteToInt(data[i]) +".255" ) , 7001 );
-            ds.send(dp);
-            if(time>0){
-                try {
-                    Thread.sleep(time);
-                } catch (InterruptedException e) {
-                }
-            }
-        }
-        ds.close();
-    }
-
-
-    private String ak;
-    public HekrConfig(String accesskey){
-        this.ak = accesskey;
-    }
-    private boolean isConfigEnd=false;
-
-    public void stop(){
-        isConfigEnd = true;
-    }
-
-    public Object config(String ssid,String password){
-        isConfigEnd = false;
-        // 100 count loop send  udpdata
-
-        for(int i = 0;i<100;i++){
-            try {
-                HekrConfig.hekrconfig(ssid + "", password + "" , 5 );
-            } catch (IOException e){
-            }
-        }
-
-        DatagramPacket dp = null;
-        boolean configsuccess=false;
-        int setakcount = 0;
-
-        for(int i =0 ; i<40 ;i++) {
-            try {
-                //dp = UDPConfig.discover("","",800);
-                dp = UDPConfig.waitDevice(1000);
-                String str= new String(dp.getData());
-                if(str.startsWith("(device ")) {
-                    configsuccess = true;
-                    break;
-                }else if(str.startsWith("(deviceACK ")) {
-                    configsuccess = true;
-                    setakcount = 100;
-                    break;
-                }
-            } catch (IOException e) {
-                try {
-                    dp = UDPConfig.setAccessKey( "255.255.255.255" , ak , null, 300);
-                    String str= new String(dp.getData());
-                    if(str.startsWith("(deviceACK ")) {
-                        configsuccess = true;
-                        setakcount = 100;
-                        break;
-                    }else if(str.startsWith("(device ")) {
-                        configsuccess = true;
-                        break;
-                    }
-
-                } catch (IOException e1) {
-                }
-
-                if(isConfigEnd){
-                    break;
-                }
-            }
-        }
-
-        isConfigEnd= true;
-        while( configsuccess ) {
-            //超时重新发送
-            if(setakcount>10){
-                break;
-            }
-            try {
-                dp = UDPConfig.setAccessKey(dp.getAddress().getHostAddress(), ak , null, 300);
-                break;
-            } catch (IOException ex) {
-            }
-            setakcount++;
-        }
-
-        if(configsuccess){
-            return dp;
-        }else{
-            return null;
-        }
-
-    }
-
-    public static boolean softapSetAccessKey(String accesskey){
-        String respstr = HttpUtil.doGet( "http://192.168.10.1/t/set_ak?ak="+accesskey , null);
-        return isSuccess(respstr);
-    }
-
-    public static boolean softapSetBridge(String ssid,String passwd){
-        String respstr = HttpUtil.doGet("http://192.168.10.1/t/set_bridge?ssid=" + ssid + "&key=" + passwd, null);
-        return isSuccess(respstr);
-    }
-
-    public static List softapList(){
-        String respstr = HttpUtil.doGet("http://192.168.10.1/t/get_aplist", null);
-        try {
-            JSONArray ja = JSON.parseArray(respstr);
-            return ja;
-        }catch (Exception e){
-            return null;
-        }
-    }
-
-    public static boolean isSuccess(String respstr){
-        try {
-            if (respstr != null) {
-                JSONObject jo = (JSONObject) JSONObject.parse(respstr);
-                int code = jo.getInteger("code");
-                if (code == 0) {
-                    return true;
-                }
-            }
-            return false;
-        }catch (Exception ex){
-            return false;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        for(int i =0 ;i<5000000 ; i++){
-            hekrconfig("helloworld", "test" );
-        }
-    }
+//	public static void main(String[] args) throws Exception {
+//		String ak = "azAxMWZzQmo3bHNlRytvUklYWTZsNVFKV09GeGdud29ZN1lKaXZ4bitOUnhIWDJDK0JYRHdrME5qbUQ1SGJ2aitr";
+//		HekrConfigTest hc = new HekrConfigTest(ak);
+//		long begin = System.currentTimeMillis();
+//		hc.config("HEKR-C", "56781234");
+//		System.out.println((System.currentTimeMillis() - begin) / 1000);
+//	}
 }
--- UDPConfig.java
+++ UDPConfig.java
@@ -1,83 +1,154 @@
+/**
+ *
+ * Copyright (c) Hekr.me Co.ltd Hangzhou, 2013-2015
+ * 
+ * 杭州第九区科技（氦氪科技）有限公司拥有该文件的使用、复制、修改和分发的许可权
+ * 如果你想得到更多信息，请访问 <http://www.hekr.me>
+ *
+ * Hekr Co.ltd Hangzhou owns permission to use, copy, modify and
+ * distribute this documentation.
+ * For more information, please see <http://www.hekr.me>
+ * 
+ */
+
 package com.hekr.android.app.util;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 
-
+/**
+ * 
+ * UDPConfig.java created by durianskh at Oct 29, 2015 3:14:16 PM 这里对类或者接口作简要描述
+ *
+ * @author durianskh
+ * @version 1.0
+ * 
+ */
 public class UDPConfig {
 
+	/**
+	 * 等待并获取设备发回的数据
+	 * 
+	 * @param timeout
+	 * @return
+	 * @throws java.io.IOException
+	 */
+	public static DatagramPacket waitDevice(int timeout) throws IOException {
+		// 创建用来发送数据报包的套接字
+		DatagramSocket ds = new DatagramSocket(10000);
+		ds.setSoTimeout(timeout);
+		byte[] bs = new byte[1024];
+		DatagramPacket dp = new DatagramPacket(bs, bs.length);
+		try {
+			ds.receive(dp);
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			ds.close();
+		}
+		return dp;
+	}
 
-    public static DatagramPacket waitDevice(int timeout)throws IOException{
-        DatagramSocket ds = new DatagramSocket(10000); // 创建用来发送数据报包的套接字
-        //ds.setBroadcast(true);
-        ds.setSoTimeout(timeout);
-        byte[] bs = new byte[1024];
-        DatagramPacket dp = new DatagramPacket(bs, bs.length);
-        try {
-            ds.receive(dp);
-        }catch (IOException ex){
-            throw ex;
-        }finally {
-            ds.close();
-        }
-        return dp;
-    }
+	/**
+	 * 发现设备
+	 * 
+	 * @param tid
+	 * @param option
+	 * @param timeout
+	 * @return
+	 * @throws java.io.IOException
+	 */
+	public static DatagramPacket discover(String tid, String option, int timeout)
+			throws IOException {
+		// 创建用来发送数据报包的套接字
+		DatagramSocket ds = new DatagramSocket();
+		ds.setSoTimeout(timeout);
+		String data = "(discover \"" + tid + "\" \"" + option + "\" 10000 )";
+		byte[] bs = data.getBytes();
+		DatagramPacket dp = new DatagramPacket(bs, bs.length,
+				InetAddress.getByName("255.255.255.255"), 10000);
+		ds.send(dp);
+		dp.setData(new byte[1024]);
+		ds.close();
+		return dp;
+	}
 
-    /**
-     * 发现设备
-     * @param tid
-     * @param option
-     * @param timeout
-     * @return
-     * @throws IOException
-     */
-    public static DatagramPacket discover( String tid,String option,int timeout ) throws IOException{
-        DatagramSocket ds = new DatagramSocket();// 创建用来发送数据报包的套接字
-        ds.setSoTimeout(timeout);
-        String data = "(discover \""+ tid +"\" \""+ option +"\" 10000 )";
-        byte[] bs = data.getBytes();
-        DatagramPacket dp = new DatagramPacket( bs , bs.length , InetAddress.getByName("255.255.255.255" ) , 10000 );
-        ds.send(dp);
-        dp.setData(new byte[1024]);
-        //ds.receive(dp);
-        ds.close();
-        return dp;
-    }
-    /**
-     * 设置accesskey
-     * @param accesskey
-     * @param tid 可以为空，则设置所有设备
-     * @throws IOException
-     */
-    public static DatagramPacket setAccessKey(String ip,String accesskey,String tid,int timeout) throws IOException{
-        DatagramSocket ds = new DatagramSocket(10000);// 创建用来发送数据报包的套接字
-        String data = "";
-        ds.setSoTimeout(timeout);
-        if(tid == null){
-            data= "(ak \""+ accesskey +"\" )";
-        }else{
-            data =  "(ak \""+ accesskey +"\" \""+tid+"\" )";
-        }
-        byte[] bs = data.getBytes();
-        DatagramPacket dp = null;
-        try {
-            dp= new DatagramPacket(bs, bs.length, InetAddress.getByName(ip), 10000);
-            ds.send(dp);
-            dp.setData(new byte[1024]);
-            ds.receive(dp);
-        }catch (IOException ex){
-            throw ex;
-        }finally {
-            ds.close();
-        }
-        return dp;
-    }
+	/**
+	 * 
+	 * @param ssid
+	 * @param password
+	 * @param time
+	 *            每次发送之后休眠的时间，以免发送速度过快，路由器承受不住
+	 * @throws java.io.IOException
+	 */
+	public static void hekrconfig(String ssid, String password, int time) throws IOException {
+		// 创建用来发送数据报包的套接字
+		DatagramSocket ds = new DatagramSocket();
+		byte[] ssidbs = ssid.getBytes("utf-8");
+		byte[] passbs = password.getBytes("utf-8");
+		int len = ssidbs.length + passbs.length + 2;
+		byte[] d = "hekrconfig".getBytes("utf-8");
+		DatagramPacket dp;
+		dp = new DatagramPacket(d, d.length, InetAddress.getByName("224.127." + len + ".255"),
+				7001);
+		ds.send(dp);
+		ds.close();
 
-    public static void main(String[] args) throws Throwable {
-    }
+		byte[] data = (ssid + '\0' + password + '\0').getBytes();
 
+		for (int i = 0; i < data.length; i++) {
+			ds = new DatagramSocket();
+			dp = new DatagramPacket(d, d.length,
+					InetAddress.getByName("224." + i + "." + unsignedByteToInt(data[i]) + ".255"),
+					7001);
+			ds.send(dp);
+			ds.close();
+			if (time > 0) {
+				try {
+					Thread.sleep(time);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+	}
+
+	public static int unsignedByteToInt(byte b) {
+		return (int) b & 0xFF;
+	}
+
+	/**
+	 * 设置accesskey，这里只是发送ak，并不等待获取，发送之后立马返回
+	 * 
+	 * @param accesskey
+	 * @param tid
+	 *            可以为空，则设置所有设备
+	 * @throws java.io.IOException
+	 */
+	public static DatagramPacket setAccessKey(String ip, String accesskey, String tid, int timeout)
+			throws IOException {
+		// 创建用来发送数据报包的套接字
+		DatagramSocket ds = new DatagramSocket(10000);
+		String data = "";
+		// ds.setSoTimeout(timeout);
+		if (tid == null) {
+			data = "(ak \"" + accesskey + "\" )";
+		} else {
+			data = "(ak \"" + accesskey + "\" \"" + tid + "\" )";
+		}
+		byte[] bs = data.getBytes();
+		DatagramPacket dp = null;
+		try {
+
+			dp = new DatagramPacket(bs, bs.length, InetAddress.getByName(ip), 10000);
+			ds.send(dp);
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			ds.close();
+		}
+		return dp;
+	}
 
 }
